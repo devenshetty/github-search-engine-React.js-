8ea9e99bd345a590f68622e98ba4c0bd02f0d870
Deven Shetty <devenshetty123@gmail.com>
2022-01-24 13:33:05 +0530

    added pages and routes

---
+++ src/pages/About.jsx
@@ -0,0 +1,7 @@
+function Ābout(){
+  return <div>
+    <h1 className="text-5xl">This is a Github Search Engine.</h1>
+  </div>
+}
+
+export default Ābout;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,7 @@
+function Home(){
+  return <div>
+    <h1 className="text-5xl text-center">Welcome</h1>
+  </div>
+}
+
+export default Home;
