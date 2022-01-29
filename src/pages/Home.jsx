64ea1941e58222bc2cf9a03aef5bf8e9e6867650
Deven Shetty <devenshetty123@gmail.com>
2022-01-29 11:02:24 +0530

    added spinner for loading...

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,10 @@
+import UserResults from "../components/users/UserResults";
+
 function Home(){
   return <div>
-    <h1 className="text-5xl text-center">Welcome</h1>
-    {process.env.REACT_APP_GITHUB_URL}
+    <h1 className="text-5xl text-center my-12">Welcome</h1>
+     {/*insert search bar here*/}
+    <UserResults />
   </div>
 }
 
