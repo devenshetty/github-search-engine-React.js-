541f1579bc4a28ec629da17b3ca4bb482c598c80
Deven Shetty <devenshetty123@gmail.com>
2022-01-31 13:45:10 +0530

    working on the search component - incomplete

---
+++ src/components/users/UserSearch.jsx
@@ -0,0 +1,44 @@
+import { useState, useContext } from "react";
+import GithubContext from "../../context/github/GithubContext.js";
+
+function UserSearch(){
+const [text, setText] = useState("");
+
+const {users} = useContext(GithubContext);
+
+const handleChange = (e) => {
+  setText(e.target.value);
+}
+
+const handleSubmit = (e) => {
+  e.preventDefault()
+  if(text===""){
+    alert("Please enter text in the search bar");
+  }
+  else{
+    setText("");
+  }
+}
+
+  return <div className="grid sm:grid-cols-1 lg:grid-cols-1 mb-10 gap-10 m-10">
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div className="form-control">
+          <div className="relative">
+            <input type="text" className="w-full  pr-40 bg-gray-200 input input-lg text-black" placeholder="Search" value={text} onChange={handleChange} />
+            <button type="submit" className="absolute top-0 right-0 rounded-l-none w-36 btn btn-lg">
+              Go
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+    {users.length>0 && (<div>
+      <button className="btn btn-lg btn-outline">
+        clear
+      </button>
+    </div>)}
+  </div>
+}
+
+export default UserSearch;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,10 @@
 import UserResults from "../components/users/UserResults";
+import UserSearch from "../components/users/UserSearch";
 
 function Home(){
   return <div>
     <h1 className="text-5xl text-center my-12">Welcome</h1>
-     {/*insert search bar here*/}
+     <UserSearch />
     <UserResults />
   </div>
 }
